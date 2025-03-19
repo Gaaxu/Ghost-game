783907e0d01cd1e6a8d5efc7e7fd9da028d6bc8d
Gaaxu <gael13piano@gmail.com>
2025-03-19 16:59:29 +0200

    pelaaja luokka luotu

--- Public/script.js
+++ Public/script.js
@@ -1,5 +1,7 @@
-const BOARD_SIZE = 15;
-let boeard;
+const BOARD_SIZE = 20;
+const cellSize = calculateCellSize();
+let board;
+let player;
 
 document.getElementById('new-game-btn').addEventListener('click', startGame);
 
@@ -8,7 +10,9 @@ function startGame(){
     document.getElementById('intro-screen').style.display = 'none';
     document.getElementById('game-screen').style.display = 'block';
 
-    generateRandom();
+    board = generateRandom();
+
+    drawBoard(board);
 }
 
 
@@ -17,11 +21,94 @@ function generateRandom(){
     
     for(let y =0; y  < BOARD_SIZE; y++){
         for(let x =0; x  < BOARD_SIZE; x++){
-            if(y === 0){
-                newBoard[y][x] = 'W';
+            if(y === 0|| y=== BOARD_SIZE -1 || x=== 0 || x === BOARD_SIZE -1 ){
+                newBoard[y][x] = 'W'; //W tarkoittaa seinää
             }
         }
     }
 
+
+
     console.log(newBoard);
+    generateObastacles(newBoard);
+
+    const [playerX, playerY] = randomEmptyPosition(newBoard);
+    setCell(newBoard,playerX,playerY, 'P')
+    
+    return newBoard;
+}
+
+function drawBoard(board){
+    const gameBoard = document.getElementById('game-board');
+    gameBoard.style.gridTemplateColumns = `repeat(${BOARD_SIZE},1fr)`;
+
+    for(let y = 0; y < BOARD_SIZE; y ++){
+        for(let x = 0; x < BOARD_SIZE; x++){
+            const cell = document.createElement('div'); 
+            cell.classList.add('cell');
+            
+            cell.style.width = cellSize + "px";
+            cell.style.height = cellSize + "px";
+            
+            if(getCell(board,x,y) === 'W'){
+                cell.classList.add('wall');
+            }else if (getCell(board,x,y) === 'P')
+                cell.classList.add('player');
+            gameBoard.appendChild(cell);
+        }
+    }
+}
+
+function getCell(board,x,y){
+    return board[y][x];
+}
+
+function calculateCellSize(){
+    const screenSize = Math.min(window.innerWidth,window.innerHeight);
+    const gameBoardSize = 0.95 * screenSize;
+}
+
+function generateObastacles(board){
+
+    const obstacles = [
+        [[0,0],[0,1],[1,0],[1,1]], // neliö
+        [[0,0],[0,1],[0,2],[0,3]],//I
+        [[0,0],[1,0],[2,0],[3,0]],//T
+    ];
+
+    const positions = [
+        {startX: 2, startY: 2},
+        {startX: 8, startY: 2},
+        {startX: 4, startY: 8},
+        {startX: 10, startY: 10},
+        {startX: 3, startY: 16},
+    ];
+
+   
+    positions.forEach(pos=>{
+        const randomObstacle = obstacles[Math.floor(Math.random()* obstacles.length)];
+        placeObstacle(board,randomObstacle,pos.startX,pos.startY);
+    });
+}
+function placeObstacle(board, obstacle, startX, startY){
+
+    for(coordinatePair of obstacle){
+        [x,y] = coordinatePair;
+        board[startY + y][startX + x] = 'W';
+    }
+}
+
+function randomInt(min,max){
+    return Math.floor(Math.random() * (max-min + 1)) +min;
+}
+
+function randomEmptyPosition(board){
+    x = randomInt(1,BOARD_SIZE - 2);
+    y = randomInt(1,BOARD_SIZE - 2);
+
+    return [x,y];
+}
+
+function setCell(board,x,y,value){
+    board[y][x] = value;
 }
